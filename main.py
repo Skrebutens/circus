fe7a76626431e615f5524d21bf5bc9da1283821a
Skrebutens <149562137+Skrebutens@users.noreply.github.com>
2024-01-24 10:21:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,47 @@
 # Github Fork (repozitorija kopija) - https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo
 # Klonēt repozitoriju - hhttps://code.visualstudio.com/docs/sourcecontrol/intro-to-git
 
+# Specifikācija
+# - 1pt Spelētāji sāk no lauciņa nr. 1, vispār 100 lauciņu. Ir divi spēlētāji. Vinē tas kurš pirmais sasniedz pēdējo lauciņu
+# - 1pt Maksimāli - 25 raundi, ja beidzas raundi - neizšķirts
+# - 1pt Viens pēc otra met kauliņu (ar nejauša ciparu ģenerātora palidzību) un iet uz priekšu
+# - 1pt Ja trāpa uz lauciņu ar kāpnem:
+# -- zilas kāpnes ved uz leju, 18 -> 7, 67 -> 46 , 80 -> 69, 74 -> 63
+# -- sarkanas kāpnes ved uz augšu, 15 -> 24, 39 -> 48, 33 -> 52, 87 -> 96 
+# - 1pt Katrā raundā tik drukāta informācija kur atrodas spēlētājs, dators un vai ir uzkāpts uz kāpnem
+
+# Koda vertēšanas kritēriji
+# - 1pt Kodā izmanto mainīgus, ciklus (for vai while), zarošanu (if)
+# - 1pt Kods strādā bez kļūdam
+# - 1pt Mainīgo un funkciju nosaukumi atspoguļo izmantošanas būtību, bez saisinājumiem, rakstīti snake_case stilā
+# - 1pt Kodam ir jēdzīgi komentāri, pirms "if, for" koda konstrukcijam
+# - 1pt Izmaiņas saglabātas versiju vadības sistēmā Git
+
+# Dokumentācija
+# Mainīgie - https://www.w3schools.com/python/python_variables.asp
+# Operācijas ar mainīgiem - https://www.w3schools.com/python/python_operators.asp
+# Mainīgo drukāšana - https://www.w3schools.com/python/python_variables_output.asp
+# Nosacījumi, zarošana, if ... else - https://www.w3schools.com/python/python_conditions.asp
+# For cikls - https://www.w3schools.com/python/python_for_loops.asp
+# Nejauša skaitļa generēšana - https://www.w3schools.com/python/ref_random_randint.asp
+# Github Fork (repozitorija kopija) - https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo
+# Klonēt repozitoriju - hhttps://code.visualstudio.com/docs/sourcecontrol/intro-to-git
+
+player1 = 1
+player2 = 1
+
+def playerOne():
+  global player1
+  print("Your turn to go forwad")
+  dice = random.randint(1, 6)
+  print("player1 moved by forward by " , dice)
+  player1 = player1 + dice
+
+def playerTwo():
+  global player2
+  print("Your turn to go forwad")
+  dice = random.randint(1, 6)
+  player2 = player2 + dice
+  print("player1 moved by forward by " , dice)
+
+print("Artūrs Skrebutens")
